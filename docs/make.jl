f3a016756c24c9fb8cafff0994c35191127aecf5
John Washbourne <jwashbourne@chevron.com>
2020-09-19 21:58:27 +0000

    added repo to docs/make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -1,3 +1,8 @@
 using Documenter, JetPackTransforms
 
 makedocs(sitename = "JetPackTransforms", modules=[JetPackTransforms])
+
+deploydocs(
+    repo = "github.com/ChevronETC/JetPackTransforms.jl.git",
+)
+
