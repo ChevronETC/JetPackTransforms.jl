23ba26182cf8586f399824acdbaccab8680722ce
Sam Kaplan <sam.kaplan@chevron.com>
2026-01-14 15:58:43 -0600

    add time option for the slant stack

--- src/jop_slantstack.jl
+++ src/jop_slantstack.jl
@@ -1,26 +1,29 @@
 """
     A = JopSlantStack(dom[; dz=10.0, dh=10.0, h0=-1000.0, ...])
 
-where `A` is the 2D slant-stack operator mapping for `z-h` to `tau-p`.  The domain of the operator
-is `nz` x `nh` with precision T, `dz` is the depth spacing, `dh` is the half-offset spacing, and `h0` is the
-origin of the half-offset axis.  The additional named optional arguments along with their default
-values are,
+where `A` is the 2D slant-stack operator mapping for `z-h` to `tau-θ` (depth mode) or `t-h` to `tau-p` (time mode).
+The domain of the operator is `nz` x `nh` with precision T, `dz` is the depth spacing (or time interval),
+`dh` is the half-offset spacing, and `h0` is the origin of the half-offset axis.  The additional named optional arguments
+along with their default values are,
 
-* `theta=-60:1.0:60` - range of opening angles.  The ray parameter is: p=sin(theta/2)/c
-* `padz=0.0,padh=0.0` - fractional padding in depth and offset to apply before applying the Fourier transfrom
-* `taperz=(0,0)` - beginning and end taper in the z-direction before transforming from `z-h` to `kz-kh`
+* `mode="depth` - choose between "depth" and "time" to specify if the input domain is `z-h` or `t-h`.
+* `theta=-60:1.0:60` - range of opening angles used when `mode="depth"`.  The ray parameter is: p=sin(theta/2)/c
+* `p=range(-dt/dx,dt/dx,128)` - ray parameter sampling used when `mode="time"`
+* `padz=0.0,padh=0.0` - fractional padding in depth and offset to apply before applying the Fourier transform
+* `taperz=(0,0)` - beginning and end taper in the z-direction (or t-direction) before transforming from `z-h` to `kz-kh`
 * `taperh=(0,0)` - beginning and end taper in the h-direction before transforming from `z-h` to `kz-kh`
-* `taperkz=(0,0)` - beginning and end taper in the kz-direction before transforming from `kz-kh` to `z-h`
-* `taperkh=(0,0)` - beginning and end taper in the kh-direction before transforming from `kz-kh` to `z-h`
+* `taperkz=(0,0)` - beginning and end taper in the kz-direction (or frequency) before transforming from `kz-kh` to `z-h` or `f-kh` to `t-h`
+* `taperkh=(0,0)` - beginning and end taper in the kh-direction before transforming from `kz-kh` to `z-h` or `f-kh` to `t-h`
 
 # Notes
-* It should be possible to extend this operator to 3D
 
-* If your domain is t-h rather than z-h, you can still use this operator
+* It should be possible to extend this operator to 3D
+* If the mode is "time", then `padz` is the padding for the time dimension, `dz` is the time sampling interval, `taperz` is the taper for the time dimension and `tapkerkz` is the taper for frequency.
 """
 function JopSlantStack(
         dom::JetAbstractSpace{T};
         theta = collect(-60.0:1.0:60.0),
+        p = nothing,
         dz = -1.0,
         dh = -1.0,
         h0 = NaN,
@@ -29,7 +32,9 @@ function JopSlantStack(
         taperz = (0,0),
         taperh = (0,0),
         taperkz = (0,0),
-        taperkh = (0,0)) where {T}
+        taperkh = (0,0),
+        mode = "depth") where {T}
+    mode ∈ ("depth", "time") || error("expected mode to be either 'depth' or 'time', got mode=$(mode)")
     dz < 0.0 && error("expected dz>0.0, got dz=$(dz)")
     dh < 0.0 && error("expected dh>0.0, got dh=$(dh)")
     isnan(h0) && error("expected finite h0, got h0=$(h0)")
@@ -50,8 +55,13 @@ function JopSlantStack(
     nhfft = nextprod([2,3,5,7], round(Int, nh*(1+padh)))
     kh = 2 * fftfreq(nhfft, pi/dh)
 
-    # c*p
+    # c*p - used for mode=="depth"
     cp = @. sin(deg2rad(theta)/2) # factor of 1/2 is to make theta the opening angle -- i.e. go from (theta_s, theta_g)->theta
+
+    # p - used for mode=="time"
+    if p === nothing
+        p = collect(range(-dz/dh, dz/dh; length=128))
+    end
 
     # conversions
     kz,kh,cp = map(x->convert(Array{T,1}, x), (kz,kh,cp))
@@ -60,37 +70,41 @@ function JopSlantStack(
 
     # tapers
     TX = JopTaper(dom, (1,2), (taperz[1],taperh[1]), (taperz[2],taperh[2]))
-    TK = JopTaper(JetSpace(Complex{eltype(dom)},div(nzfft,2)+1,length(cp)), (1,2), (taperkz[1], taperkh[1]), (taperkz[2], taperkh[2]), mode=(:normal,:fftshift))
+    TK = JopTaper(JetSpace(Complex{eltype(dom)},div(nzfft,2)+1,mode=="time" ? length(p) : length(cp)), (1,2), (taperkz[1], taperkh[1]), (taperkz[2], taperkh[2]), mode=(:normal,:fftshift))
 
-    JopLn(dom = dom, rng = JetSpace(T, nz, length(cp)), df! = JopSlantStack_df!, df′! = JopSlantStack_df′!,
-        s = (;nzfft, nz_pad_rng, nhfft, kz, kh, cp, h0, TX, TK))
+    JopLn(dom = dom, rng = JetSpace(T, nz, mode == "time" ? length(p) : length(cp)), df! = JopSlantStack_df!, df′! = JopSlantStack_df′!,
+        s = (;mode, nzfft, nz_pad_rng, nhfft, kz, kh, cp, p, h0, TX, TK))
 end
 export JopSlantStack
 
-function JopSlantStack_df!(d::AbstractArray{T,2}, m::AbstractArray{T,2}; nzfft, nz_pad_rng, nhfft, kz, kh, cp, h0, TX, TK, kwargs...) where {T}
-    nh, np, dh = size(m,2), length(cp), abs(kh[2]-kh[1])
+function JopSlantStack_df!(d::AbstractArray{T,2}, m::AbstractArray{T,2}; mode, nzfft, nz_pad_rng, nhfft, kz, kh, cp, p, h0, TX, TK, kwargs...) where {T}
+    nh, np, dh = size(m,2), mode == "time" ? length(p) : length(cp), abs(kh[2]-kh[1])
 
     mpad = zeros(T, nzfft, nhfft)
     mpad[nz_pad_rng,1:nh] = TX*m
 
     M = rfft(mpad)
 
-    # adjoint of conjugate symmetry in kh
-    for ikz = 1:div(nzfft,2)+1
-        for ikh = 2:div(nhfft,2)
-            M[ikz,ikh] += conj(M[ikz,nhfft-ikh+2])
-            # M[ikz,ikh] += M[ikz,nhfft-ikh+2]
-        end
-        for ikh = div(nhfft,2)+2:nhfft
-            M[ikz,ikh] = 0
+    if mode == "depth"
+        # adjoint of conjugate symmetry in kh
+        for ikz = 1:div(nzfft,2)+1
+            for ikh = 2:div(nhfft,2)
+                M[ikz,ikh] += conj(M[ikz,nhfft-ikh+2])
+            end
+            for ikh = div(nhfft,2)+2:nhfft
+                M[ikz,ikh] = 0
+            end
         end
     end
 
+    compute_kh = mode == "depth" ? slantstack_compute_kh_from_kz : slantstack_compute_kh_from_frequency
+    is_out_of_bounds = mode == "depth" ? (ikh_m1, ikh_p1) -> (ikh_m1 < 1 || ikh_p1 > div(nhfft,2)+1) : (ikh_m1, ikh_p1)->(ikh_m1 < 1 || ikh_p1 > nhfft)
+
     D = zeros(eltype(M), size(M,1), np)
     for ikz = 1:div(nzfft,2)+1, ip = 1:np
-        ikh_m1, ikh_p1, _kh = slantstack_compute_kh(ikz, ip, cp, kz, kh, nhfft)
+        ikh_m1, ikh_p1, _kh = compute_kh(ikz, ip, cp, p, kz, kh, nhfft)
 
-        (ikh_m1 < 1 || ikh_p1 > div(nhfft,2)+1) && continue
+        is_out_of_bounds(ikh_m1, ikh_p1) && continue
 
         if ikh_m1 == ikh_p1
             D[ikz,ip] = M[ikz,ikh_m1]*exp(-im*kh[ikh_m1]*h0)
@@ -106,22 +120,26 @@ function JopSlantStack_df!(d::AbstractArray{T,2}, m::AbstractArray{T,2}; nzfft, 
         D[ikz,ip] = a_m1*d_m1 + a_p1*d_p1
     end
 
-    d .= brfft(TK*D, nzfft, 1)[nz_pad_rng,1:np] ./ nzfft
+    _d = brfft(TK*D, nzfft, 1)
+    d .= _d[nz_pad_rng,1:np] ./ nzfft
 end
 
-function JopSlantStack_df′!(m::AbstractArray{T,2}, d::AbstractArray{T,2}; nzfft, nz_pad_rng, nhfft, kz, kh, cp, h0, TX, TK, kwargs...) where {T}
-    nh, np, dh = size(m,2)..., length(cp), abs(kh[2]-kh[1])
+function JopSlantStack_df′!(m::AbstractArray{T,2}, d::AbstractArray{T,2}; mode, nzfft, nz_pad_rng, nhfft, kz, kh, cp, p, h0, TX, TK, kwargs...) where {T}
+    nh, np, dh = size(m,2), mode == "time" ? length(p) : length(cp), abs(kh[2]-kh[1])
 
     dpad = zeros(T, nzfft, np)
     dpad[nz_pad_rng,:] = d
 
     D = TK * (rfft(dpad, 1) ./ nzfft)
 
+    compute_kh = mode == "depth" ? slantstack_compute_kh_from_kz : slantstack_compute_kh_from_frequency
+    is_out_of_bounds = mode == "depth" ? (ikh_m1, ikh_p1) -> (ikh_m1 < 1 || ikh_p1 > div(nhfft,2)+1) : (ikh_m1, ikh_p1)->(ikh_m1 < 1 || ikh_p1 > nhfft)
+
     M = zeros(Complex{T}, div(nzfft,2)+1, nhfft)
     for ikz = 1:div(nzfft,2)+1, ip = 1:np
-        ikh_m1, ikh_p1, _kh = slantstack_compute_kh(ikz, ip, cp, kz, kh, nhfft)
+        ikh_m1, ikh_p1, _kh = compute_kh(ikz, ip, cp, p, kz, kh, nhfft)
 
-        (ikh_m1 < 1 || ikh_p1 > div(nhfft,2)+1) && continue
+        is_out_of_bounds(ikh_m1, ikh_p1) && continue
 
         if ikh_m1 == ikh_p1
             M[ikz,ikh_p1] += D[ikz,ip]*exp(im*kh[ikh_p1]*h0)
@@ -137,22 +155,35 @@ function JopSlantStack_df′!(m::AbstractArray{T,2}, d::AbstractArray{T,2}; nzff
         M[ikz,ikh_m1] += a_m1*m_m1
     end
 
-    # conjugate symmetry in kh
-    for ikz = 1:div(nzfft,2)+1, ikh = 2:div(nhfft,2)
-        M[ikz,nhfft-ikh+2] = conj(M[ikz,ikh])
-        # M[ikz,nhfft-ikh+2] = M[ikz,ikh]
+    if mode == "depth"
+        # conjugate symmetry in kh
+        for ikz = 1:div(nzfft,2)+1, ikh = 2:div(nhfft,2)
+            M[ikz,nhfft-ikh+2] = conj(M[ikz,ikh])
+        end
     end
 
     m .= TX * (brfft(M, nzfft)[nz_pad_rng,1:nh])
 end
 
-@inline function slantstack_compute_kh(ikz::Int64, ip::Int64, cp, kz, kh, nhfft)
+@inline function slantstack_compute_kh_from_kz(ikz::Int64, ip::Int64, cp, p, kz, kh, nhfft)
     _kh = 1/(1-cp[ip]^2) - 1
     _kh < 0 && return -1,-1,1.0
     _kh = 2*kz[ikz]*sqrt(_kh)
 
     ikh_m1 = floor(Int64, _kh/kh[2]) + 1
     ikh_p1 = ceil(Int64, _kh/kh[2]) + 1
+
+    ikh_m1, ikh_p1, _kh
+end
+
+@inline function slantstack_compute_kh_from_frequency(iω, ip, cp, p, ω, kh, nhfft)
+    _kh = p[ip]*ω[iω] / 2
+
+    ikh_m1 = floor(Int64, _kh/kh[2]) + 1
+    ikh_p1 = ceil(Int64, _kh/kh[2]) + 1
+
+    ikh_m1 = ikh_m1 < 1 ? nhfft + ikh_m1 : ikh_m1
+    ikh_p1 = ikh_p1 < 1 ? nhfft + ikh_p1 : ikh_p1
 
     ikh_m1, ikh_p1, _kh
 end
--- test/jop_slantstack.jl
+++ test/jop_slantstack.jl
@@ -1,5 +1,6 @@
 using JetPackTransforms, Jets, Test
 
+# depth mode
 @testset "JetSlantStack, dot product test" for T in (Float32, Float64)
     A = JopSlantStack(JetSpace(T, 64, 128); dz=10.0, dh=10.0, h0=-1000.0)
 
@@ -22,4 +23,28 @@ end
     v,i = findmax(d)
     @test i[1] == 32
     @test i[2] == findfirst(x->x≈0, state(A).cp)
+end
+
+# time mode
+@testset "JetSlantStack, dot product test" for T in (Float32, Float64)
+    A = JopSlantStack(JetSpace(T, 64, 128); dz=0.004, dh=10.0, h0=-1000.0, mode="time")
+
+    m = rand(domain(A))
+    d = A*m
+
+    lhs, rhs = dot_product_test(A,rand(domain(A)),rand(range(A)))
+    @test isapprox(lhs,rhs,rtol=1e-4)
+
+    A = JopSlantStack(JetSpace(T, 64, 128); dz=0.004, dh=10.0, h0=-1000.0, taperz=(0.3,0.3), taperh=(0.3,0.3), taperkz=(0.3,0.3), taperkh=(0.3,0.3), mode="time")
+    lhs, rhs = dot_product_test(A,rand(domain(A)),rand(range(A)))
+    @test isapprox(lhs,rhs,rtol=1e-4)
+end
+
+@testset "JetSlantStack, correctness" for padz in (0.0, 0.2)
+    A = JopSlantStack(JetSpace(Float64, 64, 128); dz=0.004, dh=10.0, h0=-1000.0, padz, mode="time")
+    m = zeros(domain(A))
+    m[32,:] .= 1
+    d = A*m
+    v,i = findmax(d)
+    @test i[1] == 32
 end
