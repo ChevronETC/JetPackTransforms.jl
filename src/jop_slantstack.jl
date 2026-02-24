b5d27c7ace1d17f7533c2bc1962710dbef6a6f5d
miladbader <milad.bader@chevron.com>
2026-02-24 16:11:10 +0000

    Add JetSlantStackShiftSum3D with irregular offsets and geologic dips

--- src/jop_slantstack.jl
+++ src/jop_slantstack.jl
@@ -6,8 +6,8 @@ The domain of the operator is `nz` x `nh` with precision T, `dz` is the depth sp
 `dh` is the offset spacing, and `h0` is the origin of the offset axis.  The additional named optional arguments
 along with their default values are,
 
-* `mode="depth` - choose between "depth" and "time" to specify if the input domain is `z-h` or `t-h`.
-* `theta=-60:1.0:60` - range of incidence angles used when `mode="depth"`.
+* `mode="depth` - choose between "depth" and "time" to specify if the input domain is `z-h` or `t-h`
+* `theta=-45:1.0:45` - range of incidence angles used when `mode="depth"`
 * `p=range(-dz/dh,dz/dh,128)` - ray parameter sampling used when `mode="time"`
 * `padz=0.0,padh=0.0` - fractional padding in depth and offset to apply before applying the Fourier transform
 * `taperz=(0,0)` - beginning and end taper (fractional) in the z-direction (or t-direction) before transforming from `z-h` to `kz-kh` or `t-h` to `f-kh`
@@ -176,8 +176,8 @@ The domain of the operator is `nz` x `nh` with precision T, `dz` is the depth sp
 `h`, if provided, is the array of offsets (can be irregular). If not provided, a regular grid is assumed with same sampling as dz.
 The additional named optional arguments along with their default values are,
 
-* `mode="depth` - choose between "depth" and "time" to specify if the input domain is `z-h` or `t-h`.
-* `theta=-60:1.0:60` - range of incidence angles used when `mode="depth"`.
+* `mode="depth` - choose between "depth" and "time" to specify if the input domain is `z-h` or `t-h`
+* `theta=-45:1.0:45` - range of incidence angles used when `mode="depth"`
 * `p=range(-dz/max(diff(h)),dz/max(diff(h)),128)` - ray parameter sampling used when `mode="time"`
 * `taperz=(0,0)` - beginning and end taper (fractional) in the z-direction (or t-direction) before shifting and summing
 
@@ -234,8 +234,10 @@ function JopSlantStackShiftSum_df!(d::AbstractArray{T,2}, m::AbstractArray{T,2};
         holder = zeros(T, nz)
         for ih = 1:nh
             shift = + h[ih] * p[ip] / dz
-            WaveFD.shiftforward!(WaveFD.shiftfilter(shift), holder, @view(mtap[:,ih]))
-            d[:,ip] .+= holder
+            if abs(shift) < nz
+                WaveFD.shiftforward!(WaveFD.shiftfilter(shift), holder, @view(mtap[:,ih]))
+                d[:,ip] .+= holder
+            end
         end
     end
     d
@@ -244,19 +246,271 @@ end
 function JopSlantStackShiftSum_df′!(m::AbstractArray{T,2}, d::AbstractArray{T,2}; dz, h, p, TZ, kwargs...) where {T}
     nz, nh, np = size(m,1), size(m,2), length(p)
 
-    @show length(h), length(p)
+    mtap = zeros(T, nz, nh)
+    @threads for ih = 1:nh
+        holder = zeros(T, nz)
+        for ip = 1:np
+            shift = + h[ih] * p[ip] / dz
+            if abs(shift) < nz
+                WaveFD.shiftadjoint!(WaveFD.shiftfilter(shift), holder, @view(d[:,ip]))
+                @views mtap[:,ih] .+= holder
+            end
+        end
+    end
+    m = TZ' * mtap
+    m
+end
+
+
+
+
+"""
+    A = JopSlantStackShiftSum3D(dom[; dz=1.0, ...])
+
+where `A` is the 3D slant-stack operator mapping for `z-hx-hy` to `z-θ-ϕ` (depth mode) or `t-hx-hy` to `tau-px-py` (time mode).
+The slant stacking is performed directly on the input domain by shifting and summing along the offset axis.
+The domain of the operator is `nz` x `nh` (for irregular) or `nz` x `nhx` x `nhy` (for regular) with precision T, `dz` is the depth spacing (or time interval),
+`hx` and `hy`, if provided, are the arrays of offsets (can be irregular). If not provided, a regular grid is assumed with same sampling as dz.
+The additional named optional arguments along with their default values are,
+
+* `mode="depth` - choose between "depth" and "time" to specify if the input domain is `z-h` or `t-h`
+* `theta=-45:1.0:45` - range of incidence angles used when `mode="depth"`
+* `phi=0:45:135` - range of image azimuth angles used when `mode="depth"`
+* `dip=0.0` - geologic dip used when `mode="depth"`
+* `azimuth=0.0` - geologic azimuth used when `mode="depth"`
+* `px=range(-dz/max(diff(hx)),dz/max(diff(hx)),64)` - ray parameter sampling used when `mode="time"`
+* `py=range(-dz/max(diff(hy)),dz/max(diff(hy)),64)` - ray parameter sampling used when `mode="time"`
+* `taperz=(0,0)` - beginning and end taper (fractional) in the z-direction (or t-direction) before shifting and summing
+
+# Notes
+
+* If the mode is "time", then `dz` is the time sampling interval, `taperz` is the taper for the time dimension.
+* For mode="depth", typically theta needs to cover both positive and negative angles and must be < 90 degrees.
+* For mode="depth", the incidence angle depends on the geologic dip and azimuth (see 3-D Seismic Imaging by Prof. Biondo Biondi, Chapter 6). If not provided, this dependence is ignored.
+"""
+function JopSlantStackShiftSum3D(
+        dom::JetAbstractSpace{T};
+        theta = collect(-45.0:1.0:45.0),
+        phi = collect(0.0:45.0:135.0),
+        px = nothing,
+        py = nothing,
+        dz = 1.0,
+        hx = nothing,
+        hy = nothing,
+        taperz = (0,0),
+        dip = 0.0,
+        azimuth = 0.0,
+        mode = "depth") where {T}
+    mode ∈ ("depth", "time") || error("expected mode to be either 'depth' or 'time', got mode=$(mode)")
+    dz < 0.0 && error("expected dz>0.0, got dz=$(dz)")
+    if mode == "time" || all(iszero, dip)
+        dip, azimuth = 0, 0
+    end
+    (typeof(dip) != typeof(azimuth)) && error("dip and azimuth should be of the same type")
+    isa(dip, Array) && (length(dip) != length(azimuth)) && error("if dip is an array, its length=$(length(dip)) should match the length of azimuth=$(length(azimuth))")
+    isa(dip, Array) && (length(dip) != size(dom, 1)) && error("if dip is an array, its length=$(length(dip)) should match the first dimension of the domain=$(size(dom, 1))")
+
+    nd = ndims(dom) # if nd = 2, the offsets will be considered as totally irregular
+    nd ∉ (2,3) && error("expected domain to be either 2D or 3D, got ndims=$(nd)")
+    (nd == 2) && (hx === nothing || hy === nothing) && error("for 2D irregular (non-lattice) offsets, hx and hy should be provided") 
+    ndy = (nd == 2 ? 2 : 3)
+    hx === nothing && (hx = collect(0.0:dz:(size(dom,2)-1)*dz))
+    hy === nothing && (hy = collect(0.0:dz:(size(dom,3)-1)*dz))
+    length(hx) != size(dom,2) && error("length of hx=$(length(hx)) must match the number of x-offsets in the domain=$(size(dom,2))")
+    length(hy) != size(dom,ndy) && error("length of hy=$(length(hy)) must match the number of y-offsets in the domain=$(size(dom,ndy))")
+    dhxmax = maximum(diff(sort(hx)))
+    dhymax = maximum(diff(sort(hy)))
+    mode == "depth" && any(abs.(theta) .>= 90.0) && error("for mode='depth', theta values must be < 90 degrees in absolute value")
+    mode == "depth" && (any(phi .> 180.0) || any(phi .< 0.0)) && error("for mode='depth', phi values must be between 0 and 180 degrees")
+
+    nz = size(dom, 1)
+    nhx = size(dom, 2)
+    nhy = size(dom, ndy)
+
+    # tan(theta), cos(phi) - used for mode=="depth"
+    tant = @. tan(deg2rad(theta))
+    phi = @. deg2rad(phi)
+
+    # p - used for both modes
+    if mode == "depth"
+        px = - tant
+        py = phi
+    elseif px === nothing || py === nothing
+        (px === nothing) && (px = collect(range(-dz/dhxmax, dz/dhxmax; length=64)))
+        (py === nothing) && (py = collect(range(-dz/dhymax, dz/dhymax; length=64)))
+    end
+
+    # conversions
+    hx,hy,px,py = map(x->convert(Array{T,1}, x), (hx,hy,px,py))
+    if !isa(dip, Array)
+        dip = T(dip)
+        azimuth = T(azimuth)
+    else
+        dip, azimuth = map(x->convert(Array{T,1}, x), (dip, azimuth))
+    end
+    dz = T(dz)
+
+    # taper
+    TZ = JopTaper(dom, (1,), (taperz[1],), (taperz[2],))
+
+    JopLn(dom = dom, rng = JetSpace(T, nz, length(px), length(py)), df! = JopSlantStackShiftSum3D_df!, df′! = JopSlantStackShiftSum3D_df′!,
+        s = (;mode, dz, hx, hy, px, py, dip, azimuth, TZ))
+end
+export JopSlantStackShiftSum3D
+
+function JopSlantStackShiftSum3D_df!(d::AbstractArray{T,3}, m::AbstractArray{T,2}; dip, azimuth, mode, dz, hx, hy, px, py, TZ, kwargs...) where {T}
+    if isa(dip, Array)
+        JopSlantStackShiftSum3D_df_vector!(d, m; dip, azimuth, mode, dz, hx, hy, px, py, TZ)
+    else
+        JopSlantStackShiftSum3D_df_scalar!(d, m; dip, azimuth, mode, dz, hx, hy, px, py, TZ)
+    end
+end
+
+function JopSlantStackShiftSum3D_df′!(m::AbstractArray{T,2}, d::AbstractArray{T,3}; dip, azimuth, mode, dz, hx, hy, px, py, TZ, kwargs...) where {T}
+    if isa(dip, Array)
+        JopSlantStackShiftSum3D_df_vector′!(m, d; dip, azimuth, mode, dz, hx, hy, px, py, TZ, kwargs...)
+    else
+        JopSlantStackShiftSum3D_df_scalar′!(m, d; dip, azimuth, mode, dz, hx, hy, px, py, TZ, kwargs...)
+    end
+end
+
+function JopSlantStackShiftSum3D_df_scalar!(d::AbstractArray{T,3}, m::AbstractArray{T,2}; dip::Real, azimuth::Real, mode, dz, hx, hy, px, py, TZ) where {T}
+    nz, nh, npx, npy = size(m,1), size(m,2), length(px), length(py)
+
+    if mode == "time"
+        compute_shift = slantstack_shift_px_py
+    elseif dip == 0
+        compute_shift = slantstack_shift_theta_phi
+    else
+        compute_shift = slantstack_shift_theta_phi_geologic
+    end
+
+    d .= 0
+    mtap = TZ * m
+    @threads for ipx = 1:npx
+        holder = zeros(T, nz)
+        for ipy = 1:npy
+            num = tan(dip)^2 *sin(azimuth - py[ipy])*cos(azimuth - py[ipy])
+            denom = sqrt(1 + (tan(dip)*sin(azimuth - py[ipy]))^2)
+            for ih = 1:nh
+                shift = compute_shift(1, ih, ih, ipx, ipy, hx, hy, px, py, dz, num, denom)
+                if abs(shift) < nz
+                    WaveFD.shiftforward!(WaveFD.shiftfilter(shift), holder, @view(mtap[:,ih]))
+                    d[:,ipx,ipy] .+= holder
+                end
+            end
+        end
+    end
+    d
+end
+
+function JopSlantStackShiftSum3D_df_vector!(d::AbstractArray{T,3}, m::AbstractArray{T,2}; dip::AbstractArray{T,1}, azimuth::AbstractArray{T,1}, mode, dz, hx, hy, px, py, TZ) where {T}
+    nz, nh, npx, npy = size(m,1), size(m,2), length(px), length(py)
+
+    compute_shift = slantstack_shift_theta_phi_geologic
+    
+    d .= 0
+    mtap = TZ * m
+    @threads for ipx = 1:npx
+        holder = zeros(T, nz)
+        for ipy = 1:npy
+            num = tan.(dip).^2 .* sin.(azimuth .- py[ipy]) .* cos.(azimuth .- py[ipy])
+            denom = sqrt.(1 .+ (tan.(dip) .* sin.(azimuth .- py[ipy])).^2)
+            for ih = 1:nh
+                for iz = 1:nz
+                    shift = compute_shift(iz, ih, ih, ipx, ipy, hx, hy, px, py, dz, num, denom)
+                    if abs(shift) < nz 
+                        WaveFD.shiftforward!(WaveFD.shiftfilter(shift), holder, @view(mtap[:,ih]))
+                        d[iz,ipx,ipy] += holder[iz]
+                    end
+                end
+            end
+        end
+    end
+    d
+end
+
+function JopSlantStackShiftSum3D_df_scalar′!(m::AbstractArray{T,2}, d::AbstractArray{T,3}; dip::Real, azimuth::Real, mode, dz, hx, hy, px, py, TZ, kwargs...) where {T}
+    nz, nh, npx, npy = size(m,1), size(m,2), length(px), length(py)
+
+    if mode == "time"
+        compute_shift = slantstack_shift_px_py
+    elseif dip == 0
+        compute_shift = slantstack_shift_theta_phi
+    else
+        compute_shift = slantstack_shift_theta_phi_geologic
+    end
 
     mtap = zeros(T, nz, nh)
     @threads for ih = 1:nh
         acc = zeros(T, nz)
         holder = zeros(T, nz)
-        for ip = 1:np
-            shift = + h[ih] * p[ip] / dz
-            WaveFD.shiftadjoint!(WaveFD.shiftfilter(shift), holder, @view(d[:,ip]))
-            acc .+= holder
+        for ipx = 1:npx
+            for ipy = 1:npy
+                num = tan(dip)^2 *sin(azimuth - py[ipy])*cos(azimuth - py[ipy])
+                denom = sqrt(1 + (tan(dip)*sin(azimuth - py[ipy]))^2)
+                shift = compute_shift(1, ih, ih, ipx, ipy, hx, hy, px, py, dz, num, denom)
+                if abs(shift) < nz
+                    WaveFD.shiftadjoint!(WaveFD.shiftfilter(shift), holder, @view(d[:,ipx,ipy]))
+                    acc .+= holder
+                end
+            end
         end
         mtap[:,ih] .= acc
     end
     m = TZ' * mtap
     m
+end
+
+function JopSlantStackShiftSum3D_df_vector′!(m::AbstractArray{T,2}, d::AbstractArray{T,3}; dip::AbstractArray{T,1}, azimuth::AbstractArray{T,1}, mode, dz, hx, hy, px, py, TZ, kwargs...) where {T}
+    nz, nh, npx, npy = size(m,1), size(m,2), length(px), length(py)
+
+    compute_shift = slantstack_shift_theta_phi_geologic
+
+    mtap = zeros(T, nz, nh)
+    @threads for ih = 1:nh
+        acc = zeros(T, nz)
+        holder = zeros(T, nz)
+        for ipx = 1:npx
+            for ipy = 1:npy
+                num = tan.(dip).^2 .* sin.(azimuth .- py[ipy]) .* cos.(azimuth .- py[ipy])
+                denom = sqrt.(1 .+ (tan.(dip) .* sin.(azimuth .- py[ipy])).^2)
+                for iz = 1:nz
+                    shift = compute_shift(iz, ih, ih, ipx, ipy, hx, hy, px, py, dz, num, denom)
+                    if abs(shift) < nz
+                        fill!(holder, zero(T))
+                        holder[iz] = d[iz,ipx,ipy]
+                        WaveFD.shiftadjoint!(WaveFD.shiftfilter(shift), acc, holder)
+                        mtap[:,ih] .+= acc
+                    end
+                end
+            end
+        end
+    end
+    m = TZ' * mtap
+    m
+end
+
+@inline function slantstack_shift_px_py(iz::Int64, ihx::Int64, ihy::Int64, ipx::Int64, ipy::Int64, hx, hy, px, py, dz, num::Real, denom::Real)
+    # for time domain, the shift is given by (hx*px + hy*py) / dz
+    shift = + (hx[ihx] * px[ipx] + hy[ihy] * py[ipy]) / dz
+    shift
+end
+
+@inline function slantstack_shift_theta_phi(iz::Int64, ihx::Int64, ihy::Int64, ipx::Int64, ipy::Int64, hx, hy, px, py, dz, num::Real, denom::Real)
+    # for depth domain ignoring geologic dip, the shift is given by (-hx*tan(θ)*cos(ϕ) - hy*tan(θ)*sin(ϕ)) / dz
+    shift = + (hx[ihx] * px[ipx] * cos(py[ipy]) + hy[ihy] * px[ipx] * sin(py[ipy])) / dz
+    shift
+end
+
+@inline function slantstack_shift_theta_phi_geologic(iz::Int64, ihx::Int64, ihy::Int64, ipx::Int64, ipy::Int64, hx, hy, px, py, dz, num::Real, denom::Real)
+    # for depth domain accounting for geologic dip, the shift is given by (-hx*tan(θ)*(cos(ϕ)/denom + sin(ϕ)*num/denom) - hy*tan(θ)*(sin(ϕ)/denom - cos(ϕ)*num/denom)) / dz
+    shift = + hx[ihx] * px[ipx] * (cos(py[ipy]) / denom + sin(py[ipy]) * num / denom) + hy[ihy] * px[ipx] * (sin(py[ipy]) / denom - cos(py[ipy]) * num / denom)
+    shift / dz
+end
+
+@inline function slantstack_shift_theta_phi_geologic(iz::Int64, ihx::Int64, ihy::Int64, ipx::Int64, ipy::Int64, hx, hy, px, py, dz, num::AbstractArray{T,1}, denom::AbstractArray{T,1}) where {T}
+    # for depth domain accounting for geologic dip, the shift is given by (-hx*tan(θ)*(cos(ϕ)/denom + sin(ϕ)*num/denom) - hy*tan(θ)*(sin(ϕ)/denom - cos(ϕ)*num/denom)) / dz
+    shift = + hx[ihx] * px[ipx] * (cos(py[ipy]) / denom[iz] + sin(py[ipy]) * num[iz] / denom[iz]) + hy[ihy] * px[ipx] * (sin(py[ipy]) / denom[iz] - cos(py[ipy]) * num[iz] / denom[iz])
+    shift / dz
 end
--- test/jop_slantstack.jl
+++ test/jop_slantstack.jl
@@ -4,9 +4,6 @@ using JetPackTransforms, Jets, Test, LinearAlgebra
 @testset "JetSlantStack, dot product test" for T in (Float32, Float64)
     A = JopSlantStack(JetSpace(T, 64, 128); dz=10.0, dh=10.0, h0=-1000.0)
 
-    m = rand(domain(A))
-    d = A*m
-
     lhs, rhs = dot_product_test(A,rand(domain(A)),rand(range(A)))
     @test isapprox(lhs,rhs,rtol=1e-4)
 
@@ -28,9 +25,6 @@ end
 # time mode
 @testset "JetSlantStack, dot product test" for T in (Float32, Float64)
     A = JopSlantStack(JetSpace(T, 64, 128); dz=0.004, dh=10.0, h0=-1000.0, mode="time")
-
-    m = rand(domain(A))
-    d = A*m
 
     lhs, rhs = dot_product_test(A,rand(domain(A)),rand(range(A)))
     @test isapprox(lhs,rhs,rtol=1e-4)
@@ -66,24 +60,13 @@ end
     dd = Ad*m
     dt = At*m
 
-    md = Ad'*dd
-    mt = At'*dt
-
     err_d = maximum(abs.(dd .- dt)) / maximum(abs.(dd))
-    err_m = maximum(abs.(md .- mt)) / maximum(abs.(md))
-    @show err_d, err_m
+    @show err_d
     @test err_d < 1e-7
-    @test err_m < 1e-7
 end
 
 @testset "JetSlantStackShiftSum, dot product test" for T in (Float32, Float64), mode in ("depth", "time")
     A = JopSlantStackShiftSum(JetSpace(T, 64, 128); dz=10.0, mode=mode)
-
-    m = rand(domain(A))
-    d = A*m
-
-    lhs, rhs = dot_product_test(A,rand(domain(A)),rand(range(A)))
-    @test isapprox(lhs,rhs,rtol=1e-4)
 
     A = JopSlantStackShiftSum(JetSpace(T, 64, 128); dz=10.0, mode=mode, h = 10.0 .* rand(128) .- 5)
     lhs, rhs = dot_product_test(A,rand(domain(A)),rand(range(A)))
@@ -106,14 +89,10 @@ end
     dd = Ad*m
     dt = At*m
 
-    md = Ad'*dd
-    mt = At'*dt
-
     err_d = maximum(abs.(dd .- dt)) / maximum(abs.(dd))
-    err_m = maximum(abs.(md .- mt)) / maximum(abs.(md))
-    @show err_d, err_m
+
+    @show err_d
     @test err_d < 1e-7
-    @test err_m < 1e-7
 end
 
 @testset "JetSlantStack vs JetSlantStackShiftSum, parity" begin
@@ -129,13 +108,81 @@ end
     d1 = A1*m
     d2 = A2*m
 
-    m1 = A1'*d1
-    m2 = A2'*d2
-
     # check cosine similarity of the results since the two operators are not exactly the same
     err_d = dot(d1, d2) / (norm(d1) * norm(d2))
-    err_m = dot(m1, m2) / (norm(m1) * norm(m2))
-    @show err_d, err_m
+
+    @show err_d
     @test err_d > 0.95
-    @test err_m > 0.95
+end
+
+@testset "JetSlantStackShiftSum3D, dot product test, T = $(T), mode = $(mode), dip = $(dip)" for T in (Float32, Float64), mode in ("depth", "time"), (dip, azimuth) in ((0.0,0.0), (30.0, 60.0), ([0.0], [0.0]))
+    if isa(dip, Array)
+        dip = 90 .* rand(64)
+        azimuth = 360 .* rand(64)
+    end
+    A = JopSlantStackShiftSum3D(JetSpace(T, 64, 128); dz=10.0, mode=mode, hx = 10.0 .* rand(128) .- 5, hy = 10.0 .* rand(128) .- 5,  dip=dip, azimuth=azimuth)
+
+    lhs, rhs = dot_product_test(A,rand(domain(A)),rand(range(A)))
+    @test isapprox(lhs,rhs,rtol=1e-4)
+end
+
+@testset "JetSlantStackShiftSum3D, time-depth parity" begin
+    theta = collect(-45:0.5:45)
+    phi = [0.0, 90.0]
+    
+    # the ray parameters that would give the same results as theta/phi are
+    px = @. -tan(deg2rad(theta))
+    py = [0.0]
+    
+    Ad = JopSlantStackShiftSum3D(JetSpace(Float64, 64, 129); mode="depth", theta=theta, phi = phi, px=px, py=py, dz=0.01, hx=collect(-0.64:0.01:0.64), hy=collect(-0.64:0.01:0.64))
+    At1 = JopSlantStackShiftSum3D(JetSpace(Float64, 64, 129); mode="time" , theta=theta, phi = phi, px=px, py=py, dz=0.01, hx=collect(-0.64:0.01:0.64), hy=collect(-0.64:0.01:0.64))
+    At2 = JopSlantStackShiftSum3D(JetSpace(Float64, 64, 129); mode="time" , theta=theta, phi = phi, px=py, py=px, dz=0.01, hx=collect(-0.64:0.01:0.64), hy=collect(-0.64:0.01:0.64))
+    m = zeros(domain(Ad))
+    for i = 1:129
+        m[div(i,4)+20,i] = 1.0
+    end
+    m[16,64] = 1
+    m[32,96] = 1
+    dd = Ad*m
+    dt1 = At1*m
+    dt2 = At2*m
+
+    err_d1 = maximum(abs.(dd[:,:,1] .- dt1[:,:,1])) / maximum(abs.(dd[:,:,1]))
+    err_d2 = maximum(abs.(dd[:,:,2] .- dt2[:,1,:])) / maximum(abs.(dd[:,:,2]))
+
+    @show err_d1, err_d2
+    @test err_d1 < 1e-7
+    @test err_d2 < 1e-7
+end
+
+@testset "JetSlantStackShiftSum3D, dip parity" begin
+    theta = collect(-45:0.5:45)
+    phi = collect(0.0:45.0:135.0)
+    
+    A1 = JopSlantStackShiftSum3D(JetSpace(Float64, 64, 129); mode="depth", theta=theta, phi = phi, dz=0.01, hx=collect(-0.64:0.01:0.64), hy=collect(-0.64:0.01:0.64), dip = 0.0, azimuth = 0.0)
+    A2 = JopSlantStackShiftSum3D(JetSpace(Float64, 64, 129); mode="depth", theta=theta, phi = phi, dz=0.01, hx=collect(-0.64:0.01:0.64), hy=collect(-0.64:0.01:0.64), dip = zeros(64), azimuth = zeros(64))
+    m = zeros(domain(A1))
+    for i = 1:129
+        m[div(i,4)+20,i] = 1.0
+    end
+    m[16,64] = 1
+    m[32,96] = 1
+    d1 = A1*m
+    d2 = A2*m
+
+    err_d = maximum(abs.(d1 .- d2)) / maximum(abs.(d1))
+
+    @show err_d
+    @test err_d < 1e-7
+
+    A1 = JopSlantStackShiftSum3D(JetSpace(Float64, 64, 129); mode="depth", theta=theta, phi = phi, dz=0.01, hx=collect(-0.64:0.01:0.64), hy=collect(-0.64:0.01:0.64), dip = 30.0, azimuth = 45.0)
+    A2 = JopSlantStackShiftSum3D(JetSpace(Float64, 64, 129); mode="depth", theta=theta, phi = phi, dz=0.01, hx=collect(-0.64:0.01:0.64), hy=collect(-0.64:0.01:0.64), dip = 30 .+ zeros(64), azimuth = 45 .+ zeros(64))
+
+    d1 = A1*m
+    d2 = A2*m
+
+    err_d = maximum(abs.(d1 .- d2)) / maximum(abs.(d1))
+
+    @show err_d
+    @test err_d < 1e-7
 end
